97841ab93943fb221cabb98d0374afdf42a22f3c
Jyotsna Ramaswamy <jo.ramaswamy@gmail.com>
2019-05-11 22:34:24 -0400

    updated routes

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,7 @@ router.post("/login", async(req,res) => {
 		res.redirect("/private");
 	}
 	else {
-		res.render("static/login", layout: false, messages: "invalid password entered.");
+		res.render("static/login", {layout: false}, {messages: "invalid password entered."});
 	}
 });
 
@@ -55,6 +55,37 @@ router.get("/private", async(req, res) => {
 	else {
 		res.status(403).json({layout: false, messages: "you need to be logged in to see this page."});
 	}
+});
+
+//allows user to logout (logout.handlebars)
+router.get("/logout", async (req, res) => {
+	res.clearCookie("AuthCookie");
+	res.render("static/login", {layout: false, messages: "you have been logged out."})
+});
+
+//browse database (database.handlebars? in static folder maybe)
+router.get("/browse", async (req, res) => {
+	res.render("static/database", {layout: false});
+});
+
+//see specific product (product.handlebars in static folder)
+//need getProductById method
+router.get("/:id", async (req, res) => {
+	const product = await data.getProductById(req.params.id);
+	if (product) {
+		res.render("layouts/product", 
+			{
+				productName: product.productName,
+				brand: product.brand,
+				effects: product.effects,
+				price: product.price,
+				reviews: product.reviews
+			});
+	}
+	else {
+		res.status(404).json({layout: false, messages: "product not found."})
+	}
+
 });
 
 module.exports = router;
