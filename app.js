663e3da06cb20b5a87f933f4bbf4f587a6c85f0b
Jo Ramaswamy <41977400+j-swamy@users.noreply.github.com>
2019-05-18 12:26:19 -0400

    populate products database

--- app.js
+++ app.js
@@ -49,6 +49,24 @@ app.listen(3000, () => {
 
 async function main() {
     const db = await connection();
+    
+    const serum_greenteaseed = await products.createProduct("intensive hydrating serum with green tea seed", "innisfree", ["hydrating", "moisturizing"], ["pore cleaning toner with volcanic cluster", "my real squeeze mask 5-pack"], 27.00, ".../views/pages/images/greenteaserum.png");
+
+    // volcanic cluster toner
+    const toner_volcaniccluster = await products.createProduct("pore cleaning toner with volcanic cluster", "innisfree", ["toning", "pore cleaning"], ["intensive hydrating serum with green tea seed", "my real squeeze mask 5-pack"], 20.00, ".../views/pages/images/volcclustertoner.png");
+
+    const cleanser_teatree = await products.createProduct("tea tree cleanser", "the body shop", ["cleanses", "imperfection care", "toning"], ["tea tree night mask"], 14.00, ".../views/pages/images/teatreecleanser.png");
+
+    const imperialis = await products.createProduct("imperialis", "lush cosmetics", ["moisturizing"], ["breath of fresh air toner water"], 26.95, ".../views/pages/images/imperialis.jpg");
+
+    const cream_aquamax = await products.createProduct("super aqua max combination watery cream", "nature republic", ["moisturizing", "hydrating", "brightening"], 16.90, ".../views/pages/images/superaquamaxcomb.png");
+
+    const mask_teatree = await products.createProduct("tea tree night mask", "the body shop", ["imperfection care", "toning"], 22.00, ".../views/pages/images/teatreemask.png");
+
+    const sixpack = await products.createProduct("my real squeeze mask 6-pack", "innisfree", ["moisturizing", "imperfection care", "hydrating", "brightening", "toning", "anti-aging"], 10.80, ".../views/pages/images/6pack.png");
+
+    const wgpj = await products.createProduct("watermelon glow pink juice", "glow recipe", ["hydrating", "brightening"], ["intensive hydrating serum with green tea seed"], 39.00, ".../views/pages/images/wpj.png");
+
 
     try {
     } catch (e) {
